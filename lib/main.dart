e81694de0913c36185d3526d37c7ad567c9ef8fb
mj <onyangomaureen95@gmail.com>
2019-04-15 12:08:32 +0300

    place icon button at the center

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(StatefulWidget());
+  runApp(Stateful());
+}
+
+class Stateful extends StatefulWidget{
+  createState(){
+    return StatefulState();
+  }
+}
+class StatefulState extends State<Stateful>{
+Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.green),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Stateful App', style: TextStyle(color: Colors.white, fontSize: 30.0)),
+       centerTitle: true,
+        ),
+        body: Center(
+          child: IconButton(
+            icon: Icon(Icons.favorite,size: 200),
+            onPressed: (){
+              
+            },
+        ),
+      ),
+       ),
+    );
+  }
 }
