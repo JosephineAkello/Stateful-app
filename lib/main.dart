9de06dcdbb48b38212c6c3c10cef48140580181a
mj <onyangomaureen95@gmail.com>
2019-04-15 12:53:59 +0300

    setstate to change icon colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(Stateful());
 }
 
-class Stateful extends StatefulWidget{
-  createState(){
+class Stateful extends StatefulWidget {
+  createState() {
     return StatefulState();
   }
 }
-class StatefulState extends State<Stateful>{
-Widget build(BuildContext context) {
+
+class StatefulState extends State<Stateful> {
+  bool _alreadySaved = false;
+
+  Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.green),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Stateful App', style: TextStyle(color: Colors.white, fontSize: 30.0)),
-       centerTitle: true,
+          title: Text('Stateful App',
+              style: TextStyle(color: Colors.white, fontSize: 30.0)),
+          centerTitle: true,
         ),
         body: Center(
           child: IconButton(
-            icon: Icon(Icons.favorite,size: 200),
-            onPressed: (){
-              
+            icon: _alreadySaved
+                ? Icon(
+                    Icons.favorite,
+                
+                    color: Colors.yellow,
+                  )
+                : Icon(Icons.favorite, color: Colors.pink),
+            onPressed: () {
+              _alreadySaved = !_alreadySaved;
+              setState(() {});
             },
+          ),
         ),
       ),
-       ),
     );
   }
 }
